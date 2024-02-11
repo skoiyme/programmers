41643312cbf0752981aa3d7af80d790d18324e12
skoiyme <143878217+skoiyme@users.noreply.github.com>
2024-02-11 23:07:15 +0900

    [level 0] Title: 암호 해독, Time: 0.16 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
         for(int i=code-1; i<cipher.length(); i+=code) {
-        	answer+=cipher.substring(i, i+1);
+        	answer=answer.concat(cipher.substring(i, i+1));
         }
        
         return answer;
