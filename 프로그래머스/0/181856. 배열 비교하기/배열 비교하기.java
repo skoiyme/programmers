666519501c2fef8dff58590c9f672c82ac331d9b
skoiyme <143878217+skoiyme@users.noreply.github.com>
2024-02-15 00:25:59 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int num1 = 0;
+        int num2 = 0;
+        if(arr1.length < arr2.length) {
+        	return -1;
+        }
+        if(arr1.length > arr2.length) {
+        	return 1;
+        }
+        for(int i=0; i<arr1.length; i++) {
+        	num1+=arr1[i];
+        	num2+=arr2[i];
+        }
+        
+        if(num1 < num2) {
+        	answer=-1;
+        } 
+        if(num1 > num2) {
+        	answer=1;
+        }
+        		
+        
+        return answer;
+    }
+}
