e2d4ffb80d31f68302658619cccaf4279e3b2f45
김소예 <skoiyme@gmail.com>
2024-04-19 17:37:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n) {
+        int leng = (n%2 == 0) ? n/2:n/2+1 ;
+        int[] answer = new int[leng];
+		int j = 0;
+		for (int i = 1; i <= n; i+=2) {
+				answer[j++] = i;
+		}
+
+		return answer;
+    }
+}
